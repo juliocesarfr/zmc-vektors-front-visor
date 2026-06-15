2b08bd94e2628c8593879abb2f70fb605f402571
Diego Ivan alvarado vela <dialvaradov@alumno.unsm.edu.pe>
2026-06-15 17:58:34 -0500

    di-geo-15-06-17:58

---
+++ src/app/_pages/catastro/conexiones/conexiones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conexiones',
+  standalone: true,
+  imports: [],
+  templateUrl: './conexiones.component.html',
+  styleUrl: './conexiones.component.scss'
+})
+export class ConexionesComponent {
+
+}
---
+++ src/app/_pages/catastro/conexiones/conexiones.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConexionesRoutingModule } from './conexiones-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ConexionesRoutingModule
+  ]
+})
+export class ConexionesModule { }
